587da590c54a39de1a4eeb601954ae96b5e018a6
Chezemy <Chezemy@gmail.com>
2022-10-27 10:29:55 +0300

    Initial Commit

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by Pavel Panyushkin on 27.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
