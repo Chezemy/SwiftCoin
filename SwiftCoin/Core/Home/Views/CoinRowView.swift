f172e27d3bbefab4f3680ff2dd79bc76abeda23d
Chezemy <Chezemy@gmail.com>
2022-11-02 12:31:10 +0300

    initial commit

---
+++ SwiftCoin/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,66 @@
+//
+//  CoinRowView.swift
+//  SwiftCoin
+//
+//  Created by Pavel Panyushkin on 27.10.2022.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct CoinRowView: View {
+    let coin: Coin
+    var body: some View {
+        HStack {
+            //market cap rank
+            Text("\(coin.marketCapRank ?? 1)")
+                .font(.caption)
+                .foregroundColor(.gray)
+            
+            //image
+            KFImage(URL(string: coin.image))
+                .resizable()
+                .frame(width: 32, height: 32)
+                .foregroundColor(.orange)
+                .padding(.bottom, 8)
+            
+            //coin name info
+            VStack(alignment: .leading, spacing: 4) {
+                Text(coin.name)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .padding(.leading, 4)
+                
+                Text(coin.symbol.uppercased())
+                    .font(.caption)
+                    .padding(.leading, 6)
+            }
+            .padding(.leading, 2)
+            
+            Spacer()
+            
+            //coin price info
+            
+            VStack(alignment: .trailing) {
+                Text(coin.currentPrice.toCurrency())
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .padding(.leading, 4)
+                
+                Text(coin.priceChangePercentage24H.toPercentString())
+                    .font(.caption)
+                    .padding(.leading, 6)
+                    .foregroundColor(coin.priceChangePercentage24H > 0 ? .green : .red)
+            }
+            .padding(.leading, 2)
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 4)
+    }
+}
+
+//struct CoinRowView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CoinRowView()
+//    }
+//}
---
+++ SwiftCoin/Core/Home/Views/HomeView.swift
@@ -0,0 +1,33 @@
+//
+//  HomeView.swift
+//  SwiftCoin
+//
+//  Created by Pavel Panyushkin on 27.10.2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @StateObject var viewModel = HomeViewModel()
+    
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                
+                // top movers view
+                TopMoversView(viewModel: viewModel)
+                
+                Divider()
+                //all coins view
+                AllCoinsView(viewModel: viewModel)
+            }
+            .navigationTitle("Live Prices")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
